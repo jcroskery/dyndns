bb7cd4c1657140ffd51b624afd534becd5161f2a
Justus Croskery <justus@olmmcc.tk>
2019-05-20 11:28:35 -0400

    Added licence notice to dyndns.c

--- dyndns.c
+++ dyndns.c
@@ -5,6 +5,21 @@
 #include <curl/curl.h> //You must link the libcurl library or the code will fail to link
 
 /*
+Copyright (C) 2019  Justus Croskery
+To contact me, email me at justus@olmmcc.tk.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program. If not, see https://www.gnu.org/licenses/.
+*/
+
+/*
 To use this program, you must pass at least 4 command line arguments:
 
 The first: Whether you want to use the caching feature of this program to cache your Cloudflare zone id and record id(s).
